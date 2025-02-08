2018cac474b97f713ab34dddb309b2baa877e106
roghayealiizade1 <roghayealiizade1@gmail.com>
2025-02-08 15:05:56 +0330

    .

---
+++ iliap/iliaapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+import reverse
+from django.http import HttpResponse
+import requests
+
+# Create your views here.
+
+c_list=[
+    {"id": 1,"name":"pyrhon"},
+    {"id": 2,"name":"c++"},
+    {"id": 3,"name":"c"},
+    {"id": 4,"name":"c#"}
+]
+
+
+def course(request):
+    id=request.GET.get('id','0')
+    for item in c_list:
+        if item['id'] in c_list:
+            return render(request,'')
+        else :
+            return render(request,'')
+            
